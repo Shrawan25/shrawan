89134b4bb3edec1ffa9267fc704314a4a7fc2679
shrawan <53131884+Shrawan25@users.noreply.github.com>
2021-04-13 08:53:53 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Official Site of Shrawan Panthi.</title>
+
+	<link href="css/style.css" rel='stylesheet' type='text/css' />
+
+<body>
+	<body style="background-color:powderblue;">
+<div style="color:white;">Webiste is under maintenance.</div>
+</body>
+</head>
+</html>
